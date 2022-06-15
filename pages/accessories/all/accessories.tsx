050b63ee21bb04c8dcffcc7dfb9a63a7c15dba81
Henry B <noelys215@gmail.com>
2022-06-14 21:26:13 -0400

    refactored slug & started implimenting ISR

--- pages/accessories/all/accessories.tsx
+++ pages/accessories/all/accessories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Box, CircularProgress, Container, Divider, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Box, Container, Divider, Typography } from '@mui/material';
+// import { useEffect, useState } from 'react';
 import ProductsCat from '../../../components/ProductsCat';
-import Sort from '../../../components/Sort';
+// import Sort from '../../../components/Sort';
 import client from '../../../utils/client';
 
 interface ProductProps {
@@ -13,90 +13,89 @@ interface ProductProps {
 	sort?: any;
 }
 
-const Accessories: React.FC<ProductProps> = () => {
+const Accessories: React.FC<ProductProps> = ({ accessories }) => {
 	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [state, setState] = useState<ProductProps>({
-		accessories: [],
-		error: '',
-		loading: true,
-		sort: '',
-	});
+	// const [state, setState] = useState<ProductProps>({
+	// 	accessories: [],
+	// 	error: '',
+	// 	loading: true,
+	// 	sort: '',
+	// });
 
-	const { accessories, loading, error, sort } = state;
+	// const { loading, error, sort } = state;
 
-	const handleSort = (e: any) => {
-		return setState({ sort: e.target.value });
-	};
+	// const handleSort = (e: any) => {
+	// 	return setState({ sort: e.target.value });
+	// };
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const accessories = await client
-					.fetch(`*[_type == "accessories"]`)
-					.then((product) =>
-						product
-							.map((product: any) => product)
-							.sort((a: any, b: any) => {
-								if (sort === 'Lowest') {
-									return a.price - b.price;
-								} else if (sort === 'Highest') {
-									return b.price - a.price;
-								} else if (sort === '') {
-									return product;
-								} else {
-									return;
-								}
-							})
-					);
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			const accessories = await client
+	// 				.fetch(`*[_type == "accessories"]`)
+	// 				.then((product) =>
+	// 					product
+	// 						.map((product: any) => product)
+	// 						.sort((a: any, b: any) => {
+	// 							if (sort === 'Lowest') {
+	// 								return a.price - b.price;
+	// 							} else if (sort === 'Highest') {
+	// 								return b.price - a.price;
+	// 							} else if (sort === '') {
+	// 								return product;
+	// 							} else {
+	// 								return;
+	// 							}
+	// 						})
+	// 				);
 
-				setState({ accessories, loading: false, sort });
-			} catch (err: any) {
-				setState({ loading: false, error: err.message });
-			}
-		};
-		fetchData();
-	}, [error, sort]);
+	// 			setState({ accessories, loading: false, sort });
+	// 		} catch (err: any) {
+	// 			setState({ loading: false, error: err.message });
+	// 		}
+	// 	};
+	// 	fetchData();
+	// }, [error, sort]);
 
 	return (
 		<>
-			{loading ? (
-				<Container maxWidth="xl">
-					<Box
-						sx={{
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							width: '100%',
-						}}>
-						<CircularProgress />
-					</Box>
-				</Container>
-			) : (
-				<Container maxWidth="xl" sx={{ mb: 'auto' }}>
-					{/* Title */}
+			(
+			<Container maxWidth="xl" sx={{ mb: 'auto' }}>
+				{/* Title */}
 
-					<Box display={'flex'} justifyContent={'space-between'}>
-						<Typography variant="h5" mb={1}>
-							Accessories
-						</Typography>
-						<Sort handleSort={handleSort} />
-					</Box>
+				<Box display={'flex'} justifyContent={'space-between'}>
+					<Typography variant="h5" mb={1}>
+						Accessories
+					</Typography>
+					{/* <Sort handleSort={handleSort} /> */}
+				</Box>
 
-					<Divider />
+				<Divider />
 
-					{/* Test */}
-					<ProductsCat
-						department="accessories"
-						title="All Accessories"
-						products={accessories}
-						type={'accessories'}
-						cat={'all'}
-					/>
-				</Container>
-			)}
+				{/* Test */}
+				<ProductsCat
+					department="accessories"
+					title="All Accessories"
+					products={accessories}
+					type={'accessories'}
+					cat={'all'}
+				/>
+			</Container>
+			)
 		</>
 	);
 };
 
 export default Accessories;
+
+export async function getStaticProps() {
+	const accessories = await client.fetch(`*[_type == "accessories"]`);
+
+	return {
+		props: {
+			accessories,
+		},
+		revalidate: 15,
+	};
+}
--- pages/womens/tops/tops.tsx
+++ pages/womens/tops/tops.tsx
@@ -1,12 +1,12 @@
-import { Box, CircularProgress, Container, Divider, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { Box, Container, Divider, Typography } from '@mui/material';
+import React from 'react';
 import ProductsCat from '../../../components/ProductsCat';
-import Sort from '../../../components/Sort';
+// import Sort from '../../../components/Sort';
 import client from '../../../utils/client';
 
 interface ProductProps {
 	shirts?: string[];
-	shoes?: string[];
+	tanks?: string[];
 	jackets?: string[];
 	loading?: boolean;
 	error?: string;
@@ -14,130 +14,131 @@ interface ProductProps {
 	sort?: any;
 }
 
-const tops: React.FC<ProductProps> = () => {
+const WomensTops: React.FC<ProductProps> = ({ tanks, shirts, jackets }: ProductProps) => {
 	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [state, setState] = useState<ProductProps>({
-		shirts: [],
-		error: '',
-		loading: true,
-		sort: '',
-	});
+	// const [state, setState] = useState<ProductProps>({
+	// 	shirts: [],
+	// 	error: '',
+	// 	loading: true,
+	// 	sort: '',
+	// });
 
-	const { shirts, shoes, jackets, loading, error, sort } = state;
+	// const { shirts, shoes, jackets, loading, error, sort } = state;
 
-	const handleSort = (e: any) => {
-		return setState({ sort: e.target.value });
-	};
+	// const handleSort = (e: any) => {
+	// 	return setState({ sort: e.target.value });
+	// };
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const shirts = await client.fetch(`*[_type == "WomensTops"]`).then((product) =>
-					product
-						.map((product: any) => product)
-						.sort((a: any, b: any) => {
-							if (sort === 'Lowest') {
-								return a.price - b.price;
-							} else if (sort === 'Highest') {
-								return b.price - a.price;
-							} else if (sort === '') {
-								return product;
-							} else {
-								return;
-							}
-						})
-				);
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			const shirts = await client.fetch(`*[_type == "WomensTops"]`).then((product) =>
+	// 				product
+	// 					.map((product: any) => product)
+	// 					.sort((a: any, b: any) => {
+	// 						if (sort === 'Lowest') {
+	// 							return a.price - b.price;
+	// 						} else if (sort === 'Highest') {
+	// 							return b.price - a.price;
+	// 						} else if (sort === '') {
+	// 							return product;
+	// 						} else {
+	// 							return;
+	// 						}
+	// 					})
+	// 			);
 
-				const shoes = await client.fetch(`*[_type == "womensShirts"]`).then((product) =>
-					product
-						.map((product: any) => product)
-						.sort((a: any, b: any) => {
-							if (sort === 'Lowest') {
-								return a.price - b.price;
-							} else if (sort === 'Highest') {
-								return b.price - a.price;
-							} else {
-								return;
-							}
-						})
-				);
+	// 			const shoes = await client.fetch(`*[_type == "womensShirts"]`).then((product) =>
+	// 				product
+	// 					.map((product: any) => product)
+	// 					.sort((a: any, b: any) => {
+	// 						if (sort === 'Lowest') {
+	// 							return a.price - b.price;
+	// 						} else if (sort === 'Highest') {
+	// 							return b.price - a.price;
+	// 						} else {
+	// 							return;
+	// 						}
+	// 					})
+	// 			);
 
-				const jackets = await client.fetch(`*[_type == "womensJackets"]`).then((product) =>
-					product
-						.map((product: any) => product)
-						.sort((a: any, b: any) => {
-							if (sort === 'Lowest') {
-								return a.price - b.price;
-							} else if (sort === 'Highest') {
-								return b.price - a.price;
-							} else {
-								return;
-							}
-						})
-				);
+	// 			const jackets = await client.fetch(`*[_type == "womensJackets"]`).then((product) =>
+	// 				product
+	// 					.map((product: any) => product)
+	// 					.sort((a: any, b: any) => {
+	// 						if (sort === 'Lowest') {
+	// 							return a.price - b.price;
+	// 						} else if (sort === 'Highest') {
+	// 							return b.price - a.price;
+	// 						} else {
+	// 							return;
+	// 						}
+	// 					})
+	// 			);
 
-				setState({ shirts, shoes, jackets, loading: false, sort });
-			} catch (err: any) {
-				setState({ loading: false, error: err.message });
-			}
-		};
-		fetchData();
-	}, [error, sort]);
+	// 			setState({ shirts, shoes, jackets, loading: false, sort });
+	// 		} catch (err: any) {
+	// 			setState({ loading: false, error: err.message });
+	// 		}
+	// 	};
+	// 	fetchData();
+	// }, [error, sort]);
 
 	return (
 		<>
-			{loading ? (
-				<Container maxWidth="xl">
-					<Box
-						sx={{
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							width: '100%',
-						}}>
-						<CircularProgress />
-					</Box>
-				</Container>
-			) : (
-				<Container maxWidth="xl">
-					{/* Title */}
+			<Container maxWidth="xl">
+				{/* Title */}
 
-					<Box display={'flex'} justifyContent={'space-between'}>
-						<Typography variant="h5" mb={1}>
-							Womens
-						</Typography>
-						<Sort handleSort={handleSort} />
-					</Box>
+				<Box display={'flex'} justifyContent={'space-between'}>
+					<Typography variant="h5" mb={1}>
+						Womens
+					</Typography>
+					{/* <Sort handleSort={handleSort} /> */}
+				</Box>
 
-					<Divider />
+				<Divider />
 
-					{/* Test */}
-					<ProductsCat
-						title="Tanks"
-						products={shirts}
-						type={'WomensTops'}
-						cat={'tops'}
-						department="womens"
-					/>
-					<ProductsCat
-						department="womens"
-						title="Shirts"
-						products={shoes}
-						type={'womensShirts'}
-						cat={'tops'}
-					/>
-					<ProductsCat
-						department="womens"
-						title="Jackets / Sweatshirts"
-						products={jackets}
-						type={'womensJackets'}
-						cat={'tops'}
-					/>
-				</Container>
-			)}
+				{/* Test */}
+				<ProductsCat
+					title="Tanks"
+					products={tanks}
+					type={'WomensTops'}
+					cat={'tops'}
+					department="womens"
+				/>
+				<ProductsCat
+					department="womens"
+					title="Shirts"
+					products={shirts}
+					type={'womensShirts'}
+					cat={'tops'}
+				/>
+				<ProductsCat
+					department="womens"
+					title="Jackets / Sweatshirts"
+					products={jackets}
+					type={'womensJackets'}
+					cat={'tops'}
+				/>
+			</Container>
 		</>
 	);
 };
 
-export default tops;
+export default WomensTops;
+
+export async function getStaticProps() {
+	const tanks = await client.fetch(`*[_type == "WomensTops"]`);
+	const shirts = await client.fetch(`*[_type == "womensShirts"]`);
+	const jackets = await client.fetch(`*[_type == "womensJackets"]`);
+
+	return {
+		props: {
+			shirts,
+			tanks,
+			jackets,
+		},
+		revalidate: 15,
+	};
+}
